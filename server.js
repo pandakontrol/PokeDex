46b6d455cb4444fe7f2b0af00cf26d4758aee8cd
pandakontrol <kevinkontrol97@gmail.com>
2020-02-20 15:49:18 +0100

    heroki

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+port = process.env.PORT;
 // Paginas publicas (estaticas)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Escuchemos en un puerto
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(" * La PokeDex esta funcionando en http://localhost:3000");
 });
